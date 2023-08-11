727371fa15e67b04381f21b0cb94c2da12b9da9a
imrulkayessifat <kayesimrul224@gmail.com>
2023-08-11 21:47:12 +0600

    useState progressbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,65 @@
 'use client'
-import { todo } from 'node:test';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, SetStateAction } from 'react'
 
 export default function Home() {
-  const [name, setName] = useState<string>('');
-  const [arrayState, setArrayState] = useState<string[]>([]);
 
-  const addItem = (item: string) => {
-    setArrayState([...arrayState, item]);
-    setName('');
+  const [inputValue, setInputValue] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  const handleInputChange = (e: { target: { value: SetStateAction<number>; }; }) => {
+    setInputValue(e.target.value);
   };
 
-  const removeItem = (index: number) => {
-    const newArray = arrayState.filter((_, i) => i !== index);
-    setArrayState(newArray);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress < inputValue) {
+          return prevProgress + 1;
+        }else if(prevProgress>inputValue){
+          if(prevProgress != inputValue){
+            return prevProgress - 1;
+          }
+        } else {
+          clearInterval(interval);
+          return prevProgress;
+        }
+      });
+    }, 20);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [inputValue]);
 
   return (
-    <div>
-
-      <div className="flex items-center border-b py-2">
-        <input value={name} onChange={(e) => setName(e.target.value)} className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" type="text" aria-label="Full name" />
-        <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 ml-5 rounded focus:outline-none focus:ring focus:border-blue-300' onClick={() => addItem(name)}>Add Item</button>
+    <div className="container mx-auto mt-8 p-4">
+      <h1 className="text-2xl font-bold mb-4">Dynamic Progress Bar</h1>
+      <input
+        type="number"
+        min="0"
+        max="100"
+        value={inputValue}
+        onChange={handleInputChange}
+        className="border border-gray-300 px-2 py-1 rounded-md mb-4"
+      />
+      <div className="flex mb-2 items-center justify-between">
+        <div>
+          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200">
+            Progress
+          </span>
+        </div>
+        <div className="text-right">
+          <span className="text-xs font-semibold inline-block text-teal-600">
+            {progress}%
+          </span>
+        </div>
       </div>
-      <ul>
-        {arrayState.map((item, index) => (
-          <li className='flex m-3' key={index}>
-            <p className='text-gray-700 text-base leading-6'>{item}</p>
-            <button className='bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 ml-5 rounded focus:outline-none focus:ring focus:border-red-300' onClick={() => removeItem(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+      <div className="flex h-2 mb-4 relative bg-gray-200">
+        <div
+          style={{ width: `${progress}%` }}
+          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
+        ></div>
+      </div>
     </div>
   )
 }
