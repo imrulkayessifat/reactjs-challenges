a3eda37bd01f2344334281c41df86cca3742453b
imrulkayessifat <kayesimrul224@gmail.com>
2023-08-12 20:28:44 +0600

    implement react lazyloading in image with nextjs Image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,18 @@
 'use client'
-import { useEffect, useRef, useState } from "react";
+import Image from "next/image"
+import img from '@/public/image.jpg';
 export default function Home() {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  let [len,setLen] = useState<number>(0);
-  const [value,setValue] = useState<string>('');
-  const addI = () => {
-    setLen(len+1);
-  }
-  useEffect(() => {
-    let count: number = 10;
-    const interval = setInterval(() => {
-      setValue(count.toString());
-      count--;
-      if (count < 0) {
-        clearInterval(interval);
-        if (buttonRef.current) {
-          buttonRef.current.style.display = "none";
-        }
-      }
-    }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
 
   return (
-    <div className="h-screen bg-gray-200 w-full flex flex-col justify-center items-center">
-      <p className="text-red-500">{len}</p>
-      <p className="text-gray-500">{value}</p>
-      <button onClick={()=>addI()} className="w-10 h-10" ref={buttonRef}>+</button>
+    <div className=" bg-gray-200 w-full flex flex-col justify-center items-center">
+      <Image src={img} alt={""} />
+      <Image src={img} alt={""} />
+      <Image src={img} alt={""} />
+      <Image src={img} alt={""} />
+      <Image src={img} alt={""} />
+      <Image src={img} alt={""} />
+      <Image src={img} alt={""} />
     </div>
   )
 }
