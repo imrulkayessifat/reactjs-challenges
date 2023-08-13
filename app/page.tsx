891440df586b950c3413e383fa2802c487ec66c2
imrulkayessifat <kayesimrul224@gmail.com>
2023-08-13 21:53:27 +0600

    unnessesary function call solve

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,42 @@
 'use client'
-import React, { createContext, useState } from "react";
-import Child from "./component/Child";
+import { useCallback,useState } from "react";
+
+const functionsCounter = new Set();
+
+const howManyFunctionCalled = (fn) => {
+  functionsCounter.add(fn);
+  console.log(functionsCounter.size);
+};
 
 export default function Home() {
-  const [children, setChildren] = useState([3, 1, 2]);
+  const [counter, setCounter] = useState(0);
+  const [otherCounter, setOtherCounter] = useState(0);
 
-  function removeChild(removedId: number) {
-    setChildren(children.filter((id) => id !== removedId));
-  }
+  const increment = useCallback(() => {
+    setCounter(counter + 1);
+    howManyFunctionCalled(increment);
+    console.log("Inc");
+  }, [counter]);
 
+  const decrement = useCallback(() => {
+    setCounter(counter - 1);
+    howManyFunctionCalled(decrement);
+    console.log("Dec");
+  }, [counter]);
+
+  const incrementOtherCounter = useCallback(() => {
+    setOtherCounter(otherCounter - 1);
+    howManyFunctionCalled(incrementOtherCounter);
+    console.log("IncOthC");
+  }, [otherCounter]);
   return (
-    <div className='h-screen w-full flex flex-col items-center justify-center'>
-      {children.map((id) => (
-        <Child key={id} id={id} remove={removeChild} />
-      ))}
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <h3>Counter Value:{counter}</h3>
+      <h3>Other CounterValue:{otherCounter}</h3>
+      <h3>{`No of function calls: ${functionsCounter.size}`}</h3>
+      <button className="bg-red-500 rounded-full m-5" onClick={decrement}>Decrement -</button>
+      <button className="bg-green-500 rounded-full m-5" onClick={increment}>Increment +</button>
+      <button className="bg-blue-500 rounded-full m-5" onClick={incrementOtherCounter}>IncrementOtherCounter +</button>
     </div>
   );
 }
